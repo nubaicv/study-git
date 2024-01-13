9c24c4f0d179f62c6152d99ead793df41d03e167
Ricardo Ponce <ricardo.ponce@nubai.com.cv>
2024-01-13 11:09:48 -0100

    created basic website structure

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Project Study Git</title>
+    <link rel="stylesheet" href="css/main.css">
+  </head>
+  <body>
+    <h1>Projeto Study Git</h1>
+    <p>The beginning of a beautiful project about studing Git</p>
+  </body>
+</html>
