18de0acab33b88d256c693bf83c85d9d1a9814c1
Ricardo Ponce <ricardo.ponce@nubai.com.cv>
2024-01-16 16:30:56 -0100

    color of <p> changed to black, yellow background and a new Thanks text as part of the dev branch going on

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
     <h1>Projeto Study Git</h1>
     <p>The beginning of a beautiful project about studing Git</p>
     <p>A new text as part of development</p>
+    <p>A new text as part of the dev branch going on. Thanks...</p>
   </body>
 </html>
